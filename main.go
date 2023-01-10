217286e0bc4269c48632d697e751c239946d03bd
Sigumaa <66453922+Sigumaa@users.noreply.github.com>
2023-01-10 17:58:40 +0900

    feat: add kbinani/screenshot

--- main.go
+++ main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/kbinani/screenshot"
+	"image/png"
+	"os"
+)
 
+func main() {
+	n := screenshot.NumActiveDisplays()
+
+	for i := 0; i < n; i++ {
+		bounds := screenshot.GetDisplayBounds(i)
+
+		img, err := screenshot.CaptureRect(bounds)
+		if err != nil {
+			panic(err)
+		}
+		fileName := fmt.Sprintf("%d_%dx%d.png", i, bounds.Dx(), bounds.Dy())
+		file, _ := os.Create(fileName)
+		defer file.Close()
+		png.Encode(file, img)
+
+		fmt.Printf("#%d : %v \"%s\"\n", i, bounds, fileName)
+	}
 }
